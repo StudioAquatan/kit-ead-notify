6fa6aa876629c77b7b0b4e68878f73810ce418e5
f0reachARR <f0reach@f0reach.me>
2023-02-11 12:42:14 +0900

    Implement new SSO login

--- src/kit-shibboleth/index.ts
+++ src/kit-shibboleth/index.ts
@@ -16,98 +16,80 @@ export class KitShibbolethProxy {
     this.fetch = fetchFactory(this.cookie);
   }
 
-  public async loginTo(url: string, retries = 0) {
-    const pageFetch = await this.fetch(url, {
-      headers: {
-        'User-Agent': this.userAgent,
-      },
-      redirect: 'manual',
-    });
-    if (!pageFetch.headers.has('location')) {
-      if (pageFetch.status !== 200) {
-        // TODO: レスポンスが200でないと異常とみなす
-        throw new Error(
-          'login page did not return location header, but status is not 200',
-        );
-      }
-      // TODO: リダイレクトなしでログインできたとみなす
-      return;
+  private async processSaveForm(html: string, url: string) {
+    const ssoSaveFormDom = new JSDOM(html);
+    const csrfSaveToken = ssoSaveFormDom.window.document
+      .querySelector('[name="csrf_token"]')
+      ?.getAttribute('value');
+    if (!url) {
+      throw new Error('No CSRF token provided');
     }
 
-    const locationUrl = pageFetch.headers.get('location')!;
-    if (
-      !locationUrl.startsWith(
-        'https://auth.cis.kit.ac.jp/idp/profile/SAML2/Redirect/SSO',
-      )
-    ) {
-      if (retries < 2) {
-        // 中間ページの可能性を考える
-        await this.loginTo(locationUrl, retries + 1);
-        return;
-      } else {
-        throw new Error('valid url is not presented');
-      }
-    }
-
-    const ssoPreLoginRes = await this.fetch(locationUrl, {
-      headers: {
-        'User-Agent': this.userAgent,
-      },
-      redirect: 'manual',
-    });
-    if (
-      ssoPreLoginRes.status !== 302 ||
-      !ssoPreLoginRes.headers.has('location')
-    ) {
-      throw new Error('SSO redirect has not done');
-    }
-
-    const ssoLoginUrl = ssoPreLoginRes.headers.get('location')!;
-    if (
-      !ssoLoginUrl.startsWith(
-        'https://auth.cis.kit.ac.jp/idp/profile/SAML2/Redirect/SSO',
-      )
-    ) {
-      throw new Error('SSO redirect has not done (unrecognized location)');
-    }
-
-    const ssoLoginRes = await this.fetch(ssoLoginUrl, {
-      headers: {
-        'User-Agent': this.userAgent,
-      },
-      redirect: 'manual',
-    });
-
-    if (!ssoLoginRes.ok) {
-      throw new Error('SSO initial page loading failed');
-    }
-
-    const ssoPostRes = await this.fetch(ssoLoginUrl, {
+    const ssoSaveRes = await this.fetch(url, {
       headers: {
         'User-Agent': this.userAgent,
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      redirect: 'manual',
+      redirect: 'follow',
       method: 'POST',
       body: stringify({
-        j_username: this.username,
-        j_password: this.password,
+        'shib_idp_ls_success.shib_idp_session_ss': 'true',
+        'shib_idp_ls_exception.shib_idp_session_ss': '',
         _eventId_proceed: '',
+        csrf_token: csrfSaveToken,
       }),
     });
 
-    if (!ssoPostRes.ok) {
-      throw new Error('SSO POST failed');
+    if (!ssoSaveRes.ok) {
+      throw new Error('SSO Finalize failed');
     }
 
-    const ssoPostContent = await ssoPostRes.text();
+    return ssoSaveRes;
+  }
 
-    const dom = new JSDOM(ssoPostContent);
-    const callbackUrl = dom.window.window.document
+  private async processLoadForm(html: string, url: string) {
+    const ssoSaveFormDom = new JSDOM(html);
+    const csrfSaveToken = ssoSaveFormDom.window.document
+      .querySelector('[name="csrf_token"]')
+      ?.getAttribute('value');
+    if (!url) {
+      throw new Error('No CSRF token provided');
+    }
+
+    const ssoSaveRes = await this.fetch(url, {
+      headers: {
+        'User-Agent': this.userAgent,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow',
+      method: 'POST',
+      body: stringify({
+        'shib_idp_ls_success.shib_idp_session_ss': 'true',
+        'shib_idp_ls_exception.shib_idp_session_ss': '',
+        'shib_idp_ls_value.shib_idp_session_ss': '',
+        'shib_idp_ls_exception.shib_idp_persistent_ss': '',
+        'shib_idp_ls_success.shib_idp_persistent_ss': 'true',
+        'shib_idp_ls_value.shib_idp_persistent_ss': '',
+        shib_idp_ls_supported: 'true',
+        _eventId_proceed: '',
+        csrf_token: csrfSaveToken,
+      }),
+    });
+
+    if (!ssoSaveRes.ok) {
+      throw new Error('SSO Finalize failed');
+    }
+
+    return ssoSaveRes;
+  }
+
+  public async processRelayForm(html: string) {
+    const ssoPostDom = new JSDOM(html);
+    const callbackUrl = ssoPostDom.window.window.document
       .querySelector('form[action]')
       ?.getAttribute('action');
     const callbackMethod =
-      dom.window.window.document
+      ssoPostDom.window.window.document
         .querySelector('form[action]')
         ?.getAttribute('method') || 'POST';
     if (!callbackUrl) {
@@ -115,7 +97,7 @@ export class KitShibbolethProxy {
     }
 
     const params: Record<string, string> = {};
-    dom.window.window.document
+    ssoPostDom.window.window.document
       .querySelectorAll('input[type="hidden"]')
       .forEach((elem) => {
         if (elem.hasAttribute('name') && elem.hasAttribute('value')) {
@@ -131,6 +113,7 @@ export class KitShibbolethProxy {
       callbackMethod.toLowerCase() === 'get'
         ? `${callbackUrl}?${stringify(params)}`
         : callbackUrl;
+
     const serviceLoginRes = await this.fetch(actualCallbackUrl, {
       headers: {
         'User-Agent': this.userAgent,
@@ -147,5 +130,110 @@ export class KitShibbolethProxy {
     if (!serviceLoginRes.ok) {
       throw new Error('SP returned error');
     }
+
+    return { res: serviceLoginRes, actualCallbackUrl };
+  }
+
+  public async loginTo(url: string) {
+    // とりあえず最後まで
+    let ssoLoginFormRes = await this.fetch(url, {
+      headers: {
+        'User-Agent': this.userAgent,
+      },
+      redirect: 'follow',
+    });
+
+    if (
+      ssoLoginFormRes.url.includes(
+        'auth.cis.kit.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1',
+      )
+    ) {
+      const html = await ssoLoginFormRes.text();
+      ssoLoginFormRes = await this.processLoadForm(html, ssoLoginFormRes.url);
+    }
+
+    if (
+      ssoLoginFormRes.url.includes(
+        'idp.cis.kit.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=e1s1',
+      )
+    ) {
+      const html = await ssoLoginFormRes.text();
+      ssoLoginFormRes = await this.processLoadForm(html, ssoLoginFormRes.url);
+    }
+
+    if (
+      !ssoLoginFormRes.url.includes(
+        'idp.cis.kit.ac.jp/idp/profile/SAML2/Redirect/SSO?execution=',
+      )
+    ) {
+      if (ssoLoginFormRes.status !== 200) {
+        // TODO: レスポンスが200でないと異常とみなす
+        throw new Error(
+          'login page did not return location header, but status is not 200',
+        );
+      }
+      return;
+    }
+
+    // CSRF token抽出
+    const ssoLoginFormHtml = await ssoLoginFormRes.text();
+    const ssoLoginFormDom = new JSDOM(ssoLoginFormHtml);
+    const csrfLoginToken = ssoLoginFormDom.window.document
+      .querySelector('[name="csrf_token"]')
+      ?.getAttribute('value');
+    if (!csrfLoginToken) {
+      throw new Error('No CSRF token provided');
+    }
+
+    // TODO: 基本同じはず
+    const ssoLoginUrl = ssoLoginFormRes.url;
+
+    const ssoSaveFormRes = await this.fetch(ssoLoginUrl, {
+      headers: {
+        'User-Agent': this.userAgent,
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: 'follow',
+      method: 'POST',
+      body: stringify({
+        j_username: this.username,
+        j_password: this.password,
+        _eventId_proceed: '',
+        csrf_token: csrfLoginToken,
+      }),
+    });
+
+    if (!ssoSaveFormRes.ok) {
+      throw new Error('SSO Login failed');
+    }
+
+    // localStorageチェック画面(idp.cis)
+    const ssoSaveHtml = await ssoSaveFormRes.text();
+    const ssoSaveUrl = ssoSaveFormRes.url;
+
+    const ssoPostRes = await this.processSaveForm(ssoSaveHtml, ssoSaveUrl);
+
+    // 最終画面(idp.cis)
+    const ssoPostHtml = await ssoPostRes.text();
+    const { res: authSaveRes, actualCallbackUrl } = await this.processRelayForm(
+      ssoPostHtml,
+    );
+
+    if (
+      !actualCallbackUrl.includes(
+        'auth.cis.kit.ac.jp/idp/profile/Authn/SAML2/POST/SSO',
+      )
+    ) {
+      // 多段relayじゃない
+      return;
+    }
+
+    // localStorageチェック画面(auth.cis)
+    const authSaveHtml = await authSaveRes.text();
+    const authSaveUrl = authSaveRes.url;
+
+    const authPostRes = await this.processSaveForm(authSaveHtml, authSaveUrl);
+
+    await this.processRelayForm(await authPostRes.text());
   }
 }
